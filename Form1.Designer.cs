eaefeffea0aadde134cc88819691c19316530794
RudyMeijer <RudyMeijer@gmail.com>
2017-10-01 22:29:51 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace ReferencedAssemblies
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.txtAssemblyFilename = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.chkHide = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnOpenFile);
+            this.groupBox1.Controls.Add(this.txtAssemblyFilename);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(449, 54);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Find all references for this assembly (ddl or exe)";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenFile.Location = new System.Drawing.Point(419, 17);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(24, 23);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "...";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // txtAssemblyFilename
+            // 
+            this.txtAssemblyFilename.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAssemblyFilename.Location = new System.Drawing.Point(6, 19);
+            this.txtAssemblyFilename.Name = "txtAssemblyFilename";
+            this.txtAssemblyFilename.Size = new System.Drawing.Size(407, 20);
+            this.txtAssemblyFilename.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Location = new System.Drawing.Point(12, 72);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(449, 149);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Location = new System.Drawing.Point(410, 227);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(51, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Find Referenced Assemblies";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Visible = false;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "assemblies (dll,exe)|*.dll;*.exe|all files|*.*";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // chkHide
+            // 
+            this.chkHide.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkHide.AutoSize = true;
+            this.chkHide.Location = new System.Drawing.Point(12, 227);
+            this.chkHide.Name = "chkHide";
+            this.chkHide.Size = new System.Drawing.Size(137, 17);
+            this.chkHide.TabIndex = 2;
+            this.chkHide.Text = "Hide system assemblies";
+            this.chkHide.UseVisualStyleBackColor = true;
+            this.chkHide.CheckedChanged += new System.EventHandler(this.chkHide_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnOpenFile;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 262);
+            this.Controls.Add(this.chkHide);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Referenced Assemblies";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox txtAssemblyFilename;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.CheckBox chkHide;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+
+
+namespace ReferencedAssemblies
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text += " V" + Application.ProductVersion.Substring(0, 5);
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog()== DialogResult.OK)
+            {
+                txtAssemblyFilename.Text = openFileDialog1.FileName;
+                btnGo_Click(sender, e);
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            string file = txtAssemblyFilename.Text;
+
+            if (File.Exists(file))
+            {
+                errorProvider1.Clear();
+                listView1.Items.Clear();
+                foreach (var item in GetRefAssemblies(file)) 
+                {
+                    listView1.Items.Add(item);
+                }
+            }
+            else
+            {
+                errorProvider1.SetError(txtAssemblyFilename, "Please enter an existing assembly name.");
+            }
+        }
+
+        private List<string> GetRefAssemblies(string file)
+        {
+            List<string> list = new List<string>();
+            Assembly assembly = Assembly.LoadFile(file);
+            AssemblyName[] anames = assembly.GetReferencedAssemblies();
+            foreach (AssemblyName item in anames) if (!Hide(item.Name))
+            {
+                //todo recurse
+                list.Add(item.FullName);
+            }
+            return list;
+        }
+
+        private bool Hide(string p)
+        {
+            if (!chkHide.Checked) return false;
+            string[] hide = Properties.Settings.Default.HideAssemblies.Split(',');
+            foreach (var item in hide)
+            {
+                if (p.StartsWith(item)) return true;
+            }
+            return false;
+        }
+
+        private void chkHide_CheckedChanged(object sender, EventArgs e)
+        {
+            btnGo_Click(sender, e);
+        }
+    }
+}
